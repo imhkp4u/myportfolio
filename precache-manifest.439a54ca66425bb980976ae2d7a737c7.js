2ad69845f95121a60506ba690af23822b86e5cfa
Hemant <hemantprasad99.hp@gmail.com>
2019-02-06 05:11:06 +0530

    Updates

---
+++ precache-manifest.439a54ca66425bb980976ae2d7a737c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "ca2467ad45065111c1c2",
+    "url": "/myportfolio/static/js/main.ca2467ad.chunk.js"
+  },
+  {
+    "revision": "f85d6fcda54846d8b455",
+    "url": "/myportfolio/static/js/1.f85d6fcd.chunk.js"
+  },
+  {
+    "revision": "ca2467ad45065111c1c2",
+    "url": "/myportfolio/static/css/main.83762057.chunk.css"
+  },
+  {
+    "revision": "f85d6fcda54846d8b455",
+    "url": "/myportfolio/static/css/1.bb06439a.chunk.css"
+  },
+  {
+    "revision": "09329bba4546274a89bec770299a8848",
+    "url": "/myportfolio/index.html"
+  }
+];
